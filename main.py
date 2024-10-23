7d22684e3cee6bf356534cb93418cc055c0b3a05
Felix Henneke <felix.henneke@protonmail.com>
2024-10-23 18:34:56 +0200

    add support for flexible filtering in ranking and rewards
    also add a few more test examples

--- main.py
+++ main.py
@@ -1,107 +1,211 @@
 """This is a python script for computing winners and rewards for combinatorial auctions"""
 
-from data_fetching import fetch_solutions, fetch_solutions_batch
+from data_fetching import fetch_solutions_single, fetch_solutions_batch
 from mechanism import (
     Trade,
     Solution,
     FilterRankRewardMechanism,
     NoFilter,
     BaselineFilter,
+    TokenPairs,
+    TradedTokens,
     SingleWinner,
-    MultipleWinners,
+    TokenPairFilteringWinners,
+    SubsetFilteringWinners,
     BatchSecondPriceReward,
+    BatchOverlapSecondPriceReward,
     TokenPairImprovementReward,
 )
 
 
-solutions = [
-    Solution(
-        id="batch winner",
-        solver="solver 1",
-        score=150,
-        trades=[Trade("1", "A", "B", 100), Trade("2", "A", "C", 50)],
-    ),
-    Solution(
-        id="unfair batch",
-        solver="solver 2",
-        score=110,
-        trades=[Trade("1", "A", "B", 50), Trade("2", "A", "C", 60)],
-    ),
-    Solution(
-        id="overlapping batch",
-        solver="solver 3",
-        score=100,
-        trades=[Trade("3", "B", "A", 50), Trade("2", "A", "C", 50)],
-    ),
-    Solution(
-        id="non-overlapping batch",
-        solver="solver 4",
-        score=100,
-        trades=[Trade("3", "B", "A", 40), Trade("4", "D", "E", 60)],
-    ),
-    Solution(
-        id="non-overlapping batch unfair",
-        solver="solver 5",
-        score=120,
-        trades=[Trade("3", "B", "A", 20), Trade("4", "D", "E", 100)],
-    ),
-    Solution(
-        id="reference A->B",
-        solver="solver 1",
-        score=80,
-        trades=[Trade("1", "A", "B", 80)],
-    ),
-    Solution(
-        id="reference A->C",
-        solver="solver 2",
-        score=40,
-        trades=[Trade("2", "A", "C", 40)],
-    ),
-    Solution(
-        id="runner up A->B",
-        solver="solver 2",
-        score=40,
-        trades=[Trade("1", "A", "B", 40)],
-    ),
-    Solution(
-        id="runner up A->C",
-        solver="solver 1",
-        score=40,
-        trades=[Trade("2", "A", "C", 40)],
-    ),
-    Solution(
-        id="reference B->A",
-        solver="solver 7",
-        score=30,
-        trades=[Trade("3", "B", "A", 30)],
-    ),
-    Solution(
-        id="reference F->G",
-        solver="solver 8",
-        score=50,
-        trades=[Trade("5", "F", "G", 50)],
-    ),
-    Solution(
-        id="runner up F->G",
-        solver="solver 1",
-        score=40,
-        trades=[Trade("5", "F", "G", 40)],
-    ),
-    Solution(
-        id="reference H->I",
-        solver="solver 8",
-        score=50,
-        trades=[Trade("6", "H", "I", 50)],
-    ),
+solutions_batch = [
+    [  # batch vs single order solutions
+        Solution(
+            "batch winner",
+            solver="solver 1",
+            score=200,
+            trades=[Trade("1", "A", "B", 100), Trade("2", "C", "D", 100)],
+        ),
+        Solution(
+            "best on first trade",
+            solver="solver 2",
+            score=150,
+            trades=[Trade("1", "A", "B", 150)],
+        ),
+        Solution(
+            "best on second trade",
+            solver="solver 3",
+            score=150,
+            trades=[Trade("2", "C", "D", 150)],
+        ),
+    ],
+    [  # solutions without overlap
+        Solution(
+            "best on first trade",
+            solver="solver 1",
+            score=150,
+            trades=[Trade("1", "A", "B", 150)],
+        ),
+        Solution(
+            "best on second trade",
+            solver="solver 2",
+            score=140,
+            trades=[Trade("2", "C", "D", 140)],
+        ),
+        Solution(
+            "bad batch",
+            solver="solver 3",
+            score=100,
+            trades=[Trade("1", "A", "B", 50), Trade("2", "C", "D", 50)],
+        ),
+    ],
+    [  # batch in between solutions without overlap
+        Solution(
+            "best on first trade",
+            solver="solver 1",
+            score=150,
+            trades=[Trade("1", "A", "B", 150)],
+        ),
+        Solution(
+            "batch with overlap",
+            solver="solver 3",
+            score=100,
+            trades=[Trade("1", "A", "B", 50), Trade("2", "C", "D", 50)],
+        ),
+        Solution(
+            "best on second trade",
+            solver="solver 2",
+            score=90,
+            trades=[Trade("2", "C", "D", 90)],
+        ),
+    ],
+    [  # reference is not from winner
+        Solution(
+            "batch with overlap",
+            solver="solver 1",
+            score=200,
+            trades=[Trade("1", "A", "B", 150), Trade("2", "C", "D", 50)],
+        ),
+        Solution(
+            "best on first trade",
+            solver="solver 1",
+            score=100,
+            trades=[Trade("1", "A", "B", 100)],
+        ),
+        Solution(
+            "best on second trade",
+            solver="solver 2",
+            score=90,
+            trades=[Trade("2", "C", "D", 90)],
+        ),
+    ],
+    [  # token overlap but not on the same token pair
+        Solution(
+            "batch with overlap",
+            solver="solver 1",
+            score=100,
+            trades=[Trade("1", "A", "B", 100)],
+        ),
+        Solution(
+            "best on first trade",
+            solver="solver 2",
+            score=90,
+            trades=[Trade("1", "A", "C", 90)],
+        ),
+    ],
+    [
+        Solution(
+            id="batch winner",
+            solver="solver 1",
+            score=150,
+            trades=[Trade("1", "A", "B", 100), Trade("2", "A", "C", 50)],
+        ),
+        Solution(
+            id="unfair batch",
+            solver="solver 2",
+            score=110,
+            trades=[Trade("1", "A", "B", 50), Trade("2", "A", "C", 60)],
+        ),
+        Solution(
+            id="overlapping batch",
+            solver="solver 3",
+            score=100,
+            trades=[Trade("3", "B", "A", 50), Trade("2", "A", "C", 50)],
+        ),
+        Solution(
+            id="non-overlapping batch",
+            solver="solver 4",
+            score=100,
+            trades=[Trade("3", "B", "A", 40), Trade("4", "D", "E", 60)],
+        ),
+        Solution(
+            id="non-overlapping batch unfair",
+            solver="solver 5",
+            score=120,
+            trades=[Trade("3", "B", "A", 20), Trade("4", "D", "E", 100)],
+        ),
+        Solution(
+            id="reference A->B",
+            solver="solver 1",
+            score=80,
+            trades=[Trade("1", "A", "B", 80)],
+        ),
+        Solution(
+            id="reference A->C",
+            solver="solver 2",
+            score=40,
+            trades=[Trade("2", "A", "C", 40)],
+        ),
+        Solution(
+            id="runner up A->B",
+            solver="solver 2",
+            score=40,
+            trades=[Trade("1", "A", "B", 40)],
+        ),
+        Solution(
+            id="runner up A->C",
+            solver="solver 1",
+            score=40,
+            trades=[Trade("2", "A", "C", 40)],
+        ),
+        Solution(
+            id="reference B->A",
+            solver="solver 7",
+            score=30,
+            trades=[Trade("3", "B", "A", 30)],
+        ),
+        Solution(
+            id="reference F->G",
+            solver="solver 8",
+            score=50,
+            trades=[Trade("5", "F", "G", 50)],
+        ),
+        Solution(
+            id="runner up F->G",
+            solver="solver 1",
+            score=40,
+            trades=[Trade("5", "F", "G", 40)],
+        ),
+        Solution(
+            id="reference H->I",
+            solver="solver 8",
+            score=50,
+            trades=[Trade("6", "H", "I", 50)],
+        ),
+    ],
 ]
 
 if __name__ == "__main__":
-    # solutions = fetch_solutions(
-    #     "0x659a6b86aa25c01ba6bc65d63c4204a962f91073767372aa59d89e340aec219b",
-    #     split_solutions=True,
-    #     efficiency_loss=0.01,
-    # )
-    solutions_batch = fetch_solutions_batch(9534992 - 100, 9534992)
+    # solutions_batch = [
+    #     fetch_solutions_single(
+    #         "0x659a6b86aa25c01ba6bc65d63c4204a962f91073767372aa59d89e340aec219b",
+    #         split_solutions=True,
+    #         efficiency_loss=0.01,
+    #     )
+    # ]
+    # solutions_batch = fetch_solutions_batch(9534992 - 1000, 9534992)
+
+    print(len(solutions_batch))
 
     for solutions in solutions_batch:
         print(solutions)
@@ -111,21 +215,28 @@ if __name__ == "__main__":
                 SingleWinner(),
                 BatchSecondPriceReward(12 * 10**15, 10**16),
             ),
+            # FilterRankRewardMechanism(
+            #     NoFilter(),
+            #     TokenPairFilteringWinners(),
+            #     BatchSecondPriceReward(12 * 10**15, 10**16),
+            # ),
             FilterRankRewardMechanism(
                 NoFilter(),
-                MultipleWinners(),
-                BatchSecondPriceReward(12 * 10**15, 10**16),
+                SubsetFilteringWinners(
+                    filtering_function=TradedTokens(), cumulative_filtering=True
+                ),
+                BatchOverlapSecondPriceReward(12 * 10**15, 10**16, TradedTokens()),
             ),
-            FilterRankRewardMechanism(
-                BaselineFilter(),
-                MultipleWinners(),
-                TokenPairImprovementReward(12 * 10**15, 10**16, True),
-            ),
-            FilterRankRewardMechanism(
-                BaselineFilter(),
-                MultipleWinners(),
-                TokenPairImprovementReward(12 * 10**15, 10**16, False),
-            ),
+            # FilterRankRewardMechanism(
+            #     BaselineFilter(),
+            #     TokenPairFilteringWinners(),
+            #     TokenPairImprovementReward(12 * 10**15, 10**16, True),
+            # ),
+            # FilterRankRewardMechanism(
+            #     BaselineFilter(),
+            #     TokenPairFilteringWinners(),
+            #     TokenPairImprovementReward(12 * 10**15, 10**16, False),
+            # ),
         ]:
             rewards = mechanism.winners_and_rewards(solutions)
             print(rewards)
--- mechanism.py
+++ mechanism.py
@@ -128,7 +128,7 @@ class SingleWinner(WinnerSelection):
         return [sorted(solutions, key=lambda solution: solution.score)[-1]]
 
 
-class MultipleWinners(WinnerSelection):
+class TokenPairFilteringWinners(WinnerSelection):
     single_winner_mechanism = SingleWinner()
 
     def select_winners(self, solutions: list[Solution]) -> list[Solution]:
@@ -142,6 +142,47 @@ class MultipleWinners(WinnerSelection):
             remaining_solutions = DirectedTokenPairOverlapFilter(new_winner).filter(
                 remaining_solutions
             )
+
+        return winners
+
+
+class FilterProperty(ABC):
+    @abstractmethod
+    def get_filter_set(self, solution: Solution) -> set:
+        pass
+
+
+class TokenPairs(FilterProperty):
+    def get_filter_set(self, solution: Solution) -> set:
+        return {(trade.sell_token, trade.buy_token) for trade in solution.trades}
+
+
+class TradedTokens(FilterProperty):
+    def get_filter_set(self, solution: Solution) -> set:
+        sell_tokens = {trade.sell_token for trade in solution.trades}
+        buy_tokens = {trade.buy_token for trade in solution.trades}
+        return sell_tokens.union(buy_tokens)
+
+
+@dataclass(frozen=True)
+class SubsetFilteringWinners(WinnerSelection):
+    cumulative_filtering: bool = True
+    filtering_function: FilterProperty = TradedTokens()
+
+    def select_winners(self, solutions: list[Solution]) -> list[Solution]:
+        sorted_solutions = sorted(
+            solutions, key=lambda _solution: _solution.score, reverse=True
+        )
+        winners: list[Solution] = []
+        filter_set: set[str] = set()
+        for solution in sorted_solutions:
+            solution_filter_set = self.filtering_function.get_filter_set(solution)
+            if len(solution_filter_set & filter_set) == 0:
+                winners.append(solution)
+                if not self.cumulative_filtering:
+                    filter_set = filter_set.union(solution_filter_set)
+            if self.cumulative_filtering:
+                filter_set = filter_set.union(solution_filter_set)
 
         return winners
 
@@ -191,6 +232,47 @@ class BatchSecondPriceReward(RewardMechanism):
             remaining_winners.remove(winner)
             remaining_solutions = DirectedTokenPairOverlapFilter(winner).filter(
                 remaining_solutions
+            )
+
+        return rewards
+
+
+@dataclass
+class BatchOverlapSecondPriceReward(RewardMechanism):
+    upper_cap: int
+    lower_cap: int
+    filtering_function: FilterProperty = TradedTokens()
+
+    def compute_rewards(
+        self, winners: list[Solution], solutions: list[Solution]
+    ) -> dict[str, tuple[int, int]]:
+        rewards: dict[str, tuple[int, int]] = {}
+        remaining_winners = list(winners)
+        sorted_solutions = sorted(
+            solutions, key=lambda _solution: _solution.score, reverse=True
+        )
+        for solution in sorted_solutions:
+            if solution not in winners:
+                continue
+            winner_filter_set = self.filtering_function.get_filter_set(solution)
+            reference_score = max(
+                (
+                    reference_solution.score
+                    for reference_solution in solutions
+                    if reference_solution.solver != solution.solver
+                    and reference_solution.score <= solution.score
+                    and len(
+                        winner_filter_set
+                        & self.filtering_function.get_filter_set(reference_solution)
+                    )
+                    != 0
+                ),
+                default=0,
+            )
+
+            rewards[solution.id] = (
+                min(solution.score - reference_score, self.upper_cap),
+                -min(reference_score, self.lower_cap),
             )
 
         return rewards
